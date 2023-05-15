9449f79c22a019074e6f27e4b8f824cdc5310b58
Giselle Smith <gisellec60@gmail.com>
2023-05-15 10:43:37 -0400

    test are passing

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,10 +2,7 @@ import React from "react";
 
 function Article() {
   return (
-    <div>
-      {/* please pass this test */}
-    </div>
+    <div>please pass this test</div>
   );
 }
-
 export default Article;
